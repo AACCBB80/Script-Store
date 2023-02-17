c5f6b60ce17babb8389b6060c023217c6a6137a8
AACCBB80 <89084279+AACCBB80@users.noreply.github.com>
2023-02-17 10:14:22 -0600

    added powdercoin cost

--- main.lua
+++ main.lua
@@ -558,7 +558,7 @@ new_button = function(x,y,w,h,splitx,f,f2,text,localscript)
 					script = localscripts[self.ID]
 				end
 				if script then
-					tooltip:settooltip(script["name"].." by "..script["author"].."\n\n"..script["description"])
+					tooltip:settooltip(script["name"].." by "..script["author"].." (5 PowderCoins)\n\n"..script["description"])
 				end
 				self.drawbackground = true
 			elseif tpt.mousex >= self.x2 then
